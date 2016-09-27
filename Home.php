e80f79c3904a0963064f80acb7dd99a11c5c4f79
louiselumapas <louise.lumapas.ll@gmail.com>
2016-09-27 14:23:23 +0800

    Added a remove glyphicon

--- Home.php
+++ Home.php
@@ -3,5 +3,9 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+	<button class='btn btn-danger'>
+		<i class='glyphicon glyphicon-remove'></i>
+	</button>
+
 </body>
 </html>
